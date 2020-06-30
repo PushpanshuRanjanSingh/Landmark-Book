5b76e4ac5ca02c969f422d0a9c3ebd7e12d158e8
Pushpanshu Ranjan Singh <pushpanshuranjansingh@gmail.com>
2020-06-30 23:04:22 +0530

    Initial Commit

---
+++ Landmark Book/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Landmark Book
+//
+//  Created by Pushpanshu Ranjan Singh on 30/06/20.
+//  Copyright © 2020 Pushpanshu Ranjan Singh. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
